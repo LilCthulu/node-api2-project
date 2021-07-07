d434007b919ae59dce914f4f1ec4347df2dbc220
Chaz Carbis <chazcarbis@gmail.com>
2021-07-06 23:17:27 -0500

    done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -59,4 +59,64 @@ router.post('/', async(req, res) => {
 })
 
 
+router.put('/:id', async(req, res) => {
+    try {
+        const changes = req.body;
+        const post = await Posts.update(req.params.id, changes)
+        if (!changes.title || !changes.contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: 'The post could not be found' });
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "The post information could not be modified",
+        });
+    }
+});
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: 'The Posts has been nuked' });
+            } else {
+                res.status(404).json({ message: 'The Posts could not be found' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error removing the Posts',
+            });
+        });
+});
+
+router.get('/:id/comments', async(req, res) => {
+    try {
+        const id = req.params.id
+        const posts = await Posts.findCommentById(id)
+
+        if (!posts) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(posts)
+        }
+    } catch (err) {
+        res.status(500).jsom({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+
 module.exports = router
