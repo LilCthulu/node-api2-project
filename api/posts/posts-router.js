514bf5f40b71eb25e72b7b46a8f657cd1eb62b28
Chaz Carbis <chazcarbis@gmail.com>
2021-07-06 22:40:41 -0500

    half way

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,62 @@
 // implement your posts router here
+const express = require('express');
+const Posts = require('./posts-model')
+
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.status(200).json(posts)
+
+    } catch (err) {
+        res.status(500).jsom({
+            message: "The posts information could not be retrieved"
+        });
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const posts = await Posts.findById(id)
+
+        if (!posts) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(posts)
+        }
+    } catch (err) {
+        res.status(500).jsom({
+            message: "The post information could not be retrieved"
+        })
+    }
+})
+
+router.post('/', async(req, res) => {
+    try {
+        const content = req.body
+        const newPost = await Posts.insert(content)
+        if (!content.title || !content.contents) {
+            console.log(content.title, content.contents)
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            res.status(201).json({
+                id: newPost.id,
+                title: content.title,
+                content: content.contents
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the post to the database"
+        });
+    }
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,2 +1,22 @@
 // implement your server here
 // require your posts router and connect it here
+
+// Instanciation of server
+const express = require('express');
+const server = express()
+server.use(express.json())
+
+// Instanciation of router middleware
+const postsRouter = require('./posts/posts-router')
+server.use('/api/posts', postsRouter)
+
+
+//default endpoint
+server.get('/', (req, res) => {
+    res.send(`
+     <h1>Posts API </h1>
+                `)
+})
+
+
+module.exports = server;
